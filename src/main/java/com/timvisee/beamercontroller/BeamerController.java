a3c0b23c005831706919b44a4ec4aa0d125008cc
Tim Visée <timvisee@gmail.com>
2016-11-28 00:19:15 +0100

    Create application entry point

---
+++ src/main/java/com/timvisee/beamercontroller/BeamerController.java
@@ -0,0 +1,13 @@
+package com.timvisee.beamercontroller;
+
+public class BeamerController {
+
+    /**
+     * Main method, called on start.
+     *
+     * @param args Program arguments.
+     */
+    public static void main(String[] args) {
+        System.out.println("Started BeamerController.");
+    }
+}
