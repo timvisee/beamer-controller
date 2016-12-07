4a3052b2922de6f205135c9c53c9fe76c599abc5
Tim Visée <timvisee@gmail.com>
2016-12-07 21:51:39 +0100

    Create an InterfaceType enum

---
+++ src/main/java/com/timvisee/beamercontroller/beamer/iface/InterfaceType.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) Tim Visee 2016. All rights reserved.
+ *
+ * @author Tim Visee
+ *
+ * Open Source != No Copyright
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software")
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * You should have received a copy of The MIT License (MIT) along with this
+ * program. If not, see <http://opensource.org/licenses/MIT/>.
+ *
+ */
+
+package com.timvisee.beamercontroller.beamer.iface;
+
+public enum InterfaceType {
+
+    SERIAL("Serial");
+
+    /**
+     * Interface type name.
+     */
+    public String name;
+
+    /**
+     * Constructor.
+     *
+     * @param name Interface type name.
+     */
+    InterfaceType(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Get the interface type name.
+     *
+     * @return Interface type name.
+     */
+    public String getName() {
+        return this.name;
+    }
+}
